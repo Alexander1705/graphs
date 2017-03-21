f429edcf2f8981b78f1665ee125488132c7cb48e
Alexander Movchan <alexandermovchan1705@gmail.com>
2017-03-21 13:30:29 +0200

    Add solution for 5th lab

---
+++ lab5.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from graphs import Graph
+
+filename = input('Input file: ')
+
+with open(filename) as file:
+    graph = Graph.read_adjacency_list(file, directed=True)
+
+nodes = list(graph.nodes())
+sorted_nodes = []
+while nodes:
+    for node in nodes:
+        if all(p in sorted_nodes for p in node.predecessors()):
+            sorted_nodes.append(node)
+            nodes.remove(node)
+            break
+    else:
+        print('Impossible to do top')
+        break
+else:
+    print(sorted_nodes)
